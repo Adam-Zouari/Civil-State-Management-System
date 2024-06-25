02d5cadb7e829cdbd5fa3de19692111b01b7e80c
Adam Zouari <159965292+Adam-Zouari@users.noreply.github.com>
2024-06-25 18:31:24 +0100

    Delete Java/src/tempCodeRunnerFile.java

--- Java/src/tempCodeRunnerFile.java
+++
@@ -1,2 +0,0 @@
-"SELECT Nom, Prénom, Date_de_naissance, Lieu_de_naissance, Nombre_de_retrait_total " +
-           "FROM Naissance WHERE nombre_de_retrait_total = (SELECT MIN(nombre_de_retrait_total) FROM Naissance)";
